bcffae3f7e8ca933745b89a9d3af71858cab5f36
Matthias Cullmann <matthias.cullmann@baloise.com>
2023-09-11 14:25:10 +0200

    dump JWT

--- src/main/java/com/baloise/azure/Function.java
+++ src/main/java/com/baloise/azure/Function.java
@@ -1,5 +1,8 @@
 package com.baloise.azure;
 
+import java.util.Map;
+import java.util.Optional;
+
 import com.microsoft.azure.functions.ExecutionContext;
 import com.microsoft.azure.functions.HttpMethod;
 import com.microsoft.azure.functions.HttpRequestMessage;
@@ -8,8 +11,6 @@ import com.microsoft.azure.functions.HttpStatus;
 import com.microsoft.azure.functions.annotation.AuthorizationLevel;
 import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.HttpTrigger;
-
-import java.util.Optional;
 
 /**
  * Azure Functions with HTTP Trigger.
@@ -37,7 +38,15 @@ public class Function {
         if (name == null) {
             return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass a name on the query string or in the request body").build();
         } else {
-            return request.createResponseBuilder(HttpStatus.OK).body("Hello, " + name).build();
+            return request.createResponseBuilder(HttpStatus.OK).body("Hello, " + name + " " +getJwtFromHeader(request)).build();
         }
     }
+    
+    private static String getJwtFromHeader(final HttpRequestMessage<Optional<String>> request) {
+        final Map<String, String> headers = request.getHeaders();
+        final String authHeader = headers.get("authorization");
+        final String[] authHeaderParts = authHeader.split(" ");
+        return authHeaderParts[1];
+    }
+    
 }
