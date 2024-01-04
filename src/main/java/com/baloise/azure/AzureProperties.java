f717f91b3f831ccb8025658f34f95769b7fb3b16
Matthias Cullmann <matthias.cullmann@baloise.com>
2024-01-04 15:36:06 +0100

    renamed to tenantId()

--- src/main/java/com/baloise/azure/AzureProperties.java
+++ src/main/java/com/baloise/azure/AzureProperties.java
@@ -6,6 +6,6 @@ public class AzureProperties {
 	public final static String defaultScope() {return "https://graph.microsoft.com/.default";}
 	public final static String clientId() {return "408f3c69-c6ce-42dd-8a8e-144f5e1b994e";}
 	public final static String clientSecretName() {return clientId()+"-secret";}
-	public final static String tenant() {return "eb3c68b9-0935-4046-8550-8bcaa4167e2e";}
-	public final static String authority() {return format("https://login.microsoftonline.com/%s/", tenant());}
+	public final static String tenantId() {return "eb3c68b9-0935-4046-8550-8bcaa4167e2e";}
+	public final static String authority() {return format("https://login.microsoftonline.com/%s/", tenantId());}
 }
